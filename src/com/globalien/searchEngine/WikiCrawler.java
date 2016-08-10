0ccfa98f1bef7fad92db33cc5e44c093c668b046
Ricardo De Andrade <rdeandrade95@gmail.com>
2016-08-09 23:25:19 -0400

    Added initial project files

---
+++ src/com/globalien/searchEngine/WikiCrawler.java
@@ -0,0 +1,103 @@
+package com.globalien.searchEngine;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Queue;
+
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.select.Elements;
+
+import redis.clients.jedis.Jedis;
+
+
+public class WikiCrawler {
+	// keeps track of where we started
+	private final String source;
+	
+	// the index where the results go
+	private JedisIndex index;
+	
+	// queue of URLs to be indexed
+	private Queue<String> queue = new LinkedList<String>();
+	
+	// fetcher used to get pages from Wikipedia
+	final static WikiFetcher wf = new WikiFetcher();
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param source
+	 * @param index
+	 */
+	public WikiCrawler(String source, JedisIndex index) {
+		this.source = source;
+		this.index = index;
+		queue.offer(source);
+	}
+
+	/**
+	 * Returns the number of URLs in the queue.
+	 * 
+	 * @return
+	 */
+	public int queueSize() {
+		return queue.size();	
+	}
+
+	/**
+	 * Gets a URL from the queue and indexes it.
+	 *
+	 * @return Url of the page indexed.
+	 * @throws IOException
+	 */
+	public String crawl() throws IOException {
+        String url = queue.poll();
+		if (url == null) {
+			return null;
+		}
+		if (index.isIndexed(url)) {
+			return null;
+		}
+		Elements paragraphs = wf.fetchWikipedia(url);
+		index.indexPage(url, paragraphs);
+		queueInternalLinks(paragraphs);
+		return url;
+	}
+	
+	/**
+	 * Parses paragraphs and adds internal links to the queue.
+	 * 
+	 * @param paragraphs
+	 */
+	// NOTE: absence of access level modifier means package-level
+	void queueInternalLinks(Elements paragraphs) {
+		for (Element paragraph : paragraphs) {
+			Iterable<Node> iter = new WikiNodeIterable(paragraph);
+			for (Node node : iter) {
+				if (node instanceof Element && ((Element) node).tagName().equals("a")) {
+					String link = ((Element) node).attr("href");
+					if (link.length() > 6 && link.substring(0, 6).equals("/wiki/")) {
+						queue.offer("https://en.wikipedia.org" + link);
+					}
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		
+		// make a WikiCrawler
+		Jedis jedis = JedisMaker.make();
+		JedisIndex index = new JedisIndex(jedis); 
+		String source = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+		WikiCrawler wc = new WikiCrawler(source, index);
+		
+		Map<String, Integer> map = index.getCounts("the");
+		for (Entry<String, Integer> entry: map.entrySet()) {
+			System.out.println(entry);
+		}
+	}
+}
---
+++ src/com/globalien/searchEngine/WikiSearch.java
@@ -0,0 +1,156 @@
+package com.globalien.searchEngine;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import redis.clients.jedis.Jedis;
+
+
+/**
+ * Represents the results of a search query.
+ *
+ */
+public class WikiSearch {
+	
+	// map from URLs that contain the term(s) to relevance score
+	private Map<String, Integer> map;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param map
+	 */
+	public WikiSearch(Map<String, Integer> map) {
+		this.map = map;
+	}
+	
+	/**
+	 * Looks up the relevance of a given URL.
+	 * 
+	 * @param url
+	 * @return
+	 */
+	public Integer getRelevance(String url) {
+		Integer relevance = map.get(url);
+		return relevance==null ? 0: relevance;
+	}
+	
+	/**
+	 * Prints the contents in order of term frequency.
+	 * 
+	 * @param map
+	 */
+	private  void print() {
+		List<Entry<String, Integer>> entries = sort();
+		for (Entry<String, Integer> entry: entries) {
+			System.out.println(entry);
+		}
+	}
+	
+	/**
+	 * Computes the union of two search results.
+	 * 
+	 * @param that
+	 * @return New WikiSearch object.
+	 */
+	public WikiSearch or(WikiSearch that) {
+		Map<String, Integer> thatMap = that.map;
+		Map<String, Integer> newMap = Stream.concat(map.entrySet().stream(), thatMap.entrySet().stream()).
+				collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue(),
+						(val1, val2) -> totalRelevance(val1, val2)));
+		return new WikiSearch(newMap);
+	}
+	
+	/**
+	 * Computes the intersection of two search results.
+	 * 
+	 * @param that
+	 * @return New WikiSearch object.
+	 */
+	public WikiSearch and(WikiSearch that) {
+		Map<String, Integer> thatMap = that.map;
+		Map<String, Integer> newMap = map.keySet().stream().filter(key -> thatMap.containsKey(key)).
+				collect(Collectors.toMap(key -> key, key -> totalRelevance(map.get(key), thatMap.get(key))));
+		return new WikiSearch(newMap);
+	}
+	
+	/**
+	 * Computes the intersection of two search results.
+	 * 
+	 * @param that
+	 * @return New WikiSearch object.
+	 */
+	public WikiSearch minus(WikiSearch that) {
+		Map<String, Integer> thatMap = that.map;
+		Map<String, Integer> newMap = map.keySet().stream().filter(key -> !thatMap.containsKey(key)).
+				collect(Collectors.toMap(key -> key, key -> map.get(key)));
+		return new WikiSearch(newMap);
+	}
+	
+	/**
+	 * Computes the relevance of a search with multiple terms.
+	 * 
+	 * @param rel1: relevance score for the first search
+	 * @param rel2: relevance score for the second search
+	 * @return
+	 */
+	protected int totalRelevance(Integer rel1, Integer rel2) {
+		// simple starting place: relevance is the sum of the term frequencies.
+		return rel1 + rel2;
+	}
+
+	/**
+	 * Sort the results by relevance.
+	 * 
+	 * @return List of entries with URL and relevance.
+	 */
+	public List<Entry<String, Integer>> sort() {
+		List<Entry<String, Integer>> toReturn = map.entrySet().stream().sorted((entry1, entry2) ->
+				entry1.getValue() - entry2.getValue()).collect(Collectors.toList());
+		return toReturn;
+	}
+
+	/**
+	 * Performs a search and makes a WikiSearch object.
+	 * 
+	 * @param term
+	 * @param index
+	 * @return
+	 */
+	public static WikiSearch search(String term, JedisIndex index) {
+		Map<String, Integer> map = index.getCounts(term);
+		return new WikiSearch(map);
+	}
+
+	public static void main(String[] args) throws IOException {
+		
+		// make a JedisIndex
+		Jedis jedis = JedisMaker.make();
+		JedisIndex index = new JedisIndex(jedis); 
+		
+		// search for the first term
+		String term1 = "java";
+		System.out.println("Query: " + term1);
+		WikiSearch search1 = search(term1, index);
+		search1.print();
+		
+		// search for the second term
+		String term2 = "programming";
+		System.out.println("Query: " + term2);
+		WikiSearch search2 = search(term2, index);
+		search2.print();
+		
+		// compute the intersection of the searches
+		System.out.println("Query: " + term1 + " AND " + term2);
+		WikiSearch intersection = search1.and(search2);
+		intersection.print();
+	}
+}
